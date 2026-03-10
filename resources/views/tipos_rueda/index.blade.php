0895d4fc3eb10b210426169953c747cc2475dfd6
Freddy Gonzalez <frederit@DESKTOP-LR230G0.localdomain>
2026-03-09 20:49:54 -0500

    Primer avance sistema control ruedas Laravel

---
+++ resources/views/tipos_rueda/index.blade.php
@@ -0,0 +1,21 @@
+<h1>Tipos de ruedas</h1>
+
+<form>
+    <label>Nombre del tipo de rueda</label>
+    <input type="text" placeholder="Ej: Tracción">
+    <button type="submit">Guardar</button>
+</form>
+
+<hr>
+
+<h2>Lista de tipos de ruedas</h2>
+
+<ul>
+<li>Tracción</li>
+<li>Monocaster</li>
+<li>Dual caster</li>
+<li>Rueda de carga 5x3</li>
+<li>Rueda de carga 5x4</li>
+<li>Rueda de carga 6x3</li>
+<li>Rueda de carga 6x4</li>
+</ul>
